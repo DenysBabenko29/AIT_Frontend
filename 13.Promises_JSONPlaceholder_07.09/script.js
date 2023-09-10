5fde49b0176b073b35d9d0f09e8e6893c7950b0a
Denys Babenko <denchikkooll@gamil.com>
2023-09-10 17:10:20 +0200

    lesson 13

--- 13.Promises_JSONPlaceholder_07.09/script.js
+++ 13.Promises_JSONPlaceholder_07.09/script.js
@@ -1,37 +1,63 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
-printUsers();
-
-async function printUsers() {
-    try {
-        await fetch(`${BASE_URL}/users`)
-            .then((res) => res.json())
-            .then((users) => {
-                userData = users;
-                users.forEach((user) => {
-                    const li = document.createElement("li");
-                    li.innerHTML = user.name;
-                    li.addEventListener("click", () =>
-                        displayUserDetails(user)
-                    );
-                    userList.appendChild(li);
-                });
-            });
-    } catch (error) {
+fetch(`${BASE_URL}/users`)
+    .then((res) => res.json())
+    .then((users) => {
+        userData = users;
+        users.forEach((user) => {
+            const li = document.createElement("li");
+            li.innerHTML = user.name;
+            li.addEventListener("click", () => displayUserDetails(user));
+            userList.appendChild(li);
+        });
+    })
+    .catch((err) => {
         const li = document.createElement("li");
-        li.textContent = `Пользователь не найден ${error}`;
+        li.textContent = "Пользователь не найден";
         userList.appendChild(li);
-    }
-}
+    });
 
-function displayUserDetails({name: firstName, email, phone, website, company: {name}, address : {street, city}}) {
+
+function displayUserDetails(user) {
     userInfo.innerHTML = `
-        <h2>${firstName}</h2>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Phone:</strong> ${phone}</p>
-        <p><strong>Website:</strong> ${website}</p>
-        <p><strong>Company:</strong> ${name}</p>
-        <p><strong>Address:</strong> ${street}, ${city}</p>
+        <h2>${user.name}</h2>
+        <p><strong>Email:</strong> ${user.email}</p>
+        <p><strong>Phone:</strong> ${user.phone}</p>
+        <p><strong>Website:</strong> ${user.website}</p>
+        <p><strong>Company:</strong> ${user.company.name}</p>
+        <p><strong>Address:</strong> ${user.address.street}, ${user.address.city}</p>
     `;
     userInfo.style.backgroundColor = "#f1f1f1";
 }
+
+// let userData = [];
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => res.json())
+//     .then((users) => {
+//         userData = users;
+//         users.forEach((user) => {
+//             // userList.innerHTML = `<li onclick="createUserInfo(this)">user.name</li>`
+//             const li = document.createElement("li");
+//             li.innerHTML = user.name;
+//             userList.appendChild(li);
+//         });
+//     })
+//     .catch((err) => {
+//         const li = document.createElement("li");
+//         li.textContent = "Пользователь не найден";
+//         userList.appendChild(li);
+//     });
+// // .finally(console.log('Finish'));
+
+// userList.addEventListener("click", (el) => {
+//     userData.forEach((element) => {
+//         if (el.target.textContent === element.name) {
+//             userInfo.innerHTML = `
+//             <p>Username: ${element.username}</p>
+//             <p>Email: ${element.email}</p>
+//             <p>Website: ${element.website}</p>
+//             <p>Address: ${element.address.street +", " + element.address.suite +", " + element.address.city}</p>
+//             <p>Phone: ${element.phone}</p>`
+//         }
+//     });
+// })
