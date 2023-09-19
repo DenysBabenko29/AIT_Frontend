2ecb55aa824c515e676ddd436ec110b15b6266c1
Denys Babenko <denchikkooll@gamil.com>
2023-09-19 21:12:34 +0200

    hw17

---
+++ 17_homework_18_classwork_19_09_2023/src/components/TodoList.tsx
@@ -0,0 +1,81 @@
+import React, { useState, ChangeEvent } from "react";
+
+interface ITask {
+    name: string;
+    completed: boolean;
+}
+
+const TodoList: React.FC = (): JSX.Element => {
+    const [taskList, setTaskList] = useState<ITask[]>([]);
+
+    const [task, setTask] = useState<ITask>({ name: "", completed: false });
+
+    const [check, setCheck] = useState<boolean>(false);
+
+    const AddTask = () => {
+        const taskInput = document.getElementById(
+            "taskInput"
+        ) as HTMLInputElement;
+
+        
+        if (taskInput.value.trim()) {
+            setTask({ name: taskInput.value, completed: false });
+            
+            setTaskList(prev => [...prev, task]);
+            taskInput.value = "";
+        }
+    };
+
+    const handelIsDone = (e: ChangeEvent<HTMLInputElement>) => {
+        setCheck(e.target.checked);
+        const copy = [...taskList];
+        const arr = copy.filter(task => task.name === e.target.value);
+        arr[0].completed = e.target.checked;
+        const index = copy.findIndex(task => arr[0].name = task.name);
+        copy[index] = arr[0];
+        setTaskList(copy);
+
+    }
+
+    const handelRemoveTask = (index: number): void => {
+        const updatedTask = [...taskList];
+        updatedTask.splice(index, 1);
+        setTaskList(updatedTask);
+    }
+
+    return (
+        <div className="container">
+            <h1>TODO List App</h1>
+            <div className="input-container">
+                <input
+                    className="form-label"
+                    type="text"
+                    id="taskInput"
+                    placeholder="Enter of task..."
+                />
+                <button onClick={AddTask} id="addTask">
+                    Add
+                </button>
+            </div>
+            <ul id="taskList">
+                {taskList.filter(task => task.name !== '').map((task, index) => (
+                    <li key={index}>
+                        {task.name}
+                        <input onChange={handelIsDone} type="checkbox" id="isDone" />
+                        <button onClick={() => handelRemoveTask(index)}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
+
+
+
+
+
+
