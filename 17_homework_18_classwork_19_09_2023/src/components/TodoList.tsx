c312674638f105601c2aa95c05bd57453e764aef
Denys Babenko <denchikkooll@gamil.com>
2023-09-21 09:44:29 +0200

    hw 17 todo

--- 17_homework_18_classwork_19_09_2023/src/components/TodoList.tsx
+++ 17_homework_18_classwork_19_09_2023/src/components/TodoList.tsx
@@ -17,37 +17,39 @@ const TodoList: React.FC = (): JSX.Element => {
             "taskInput"
         ) as HTMLInputElement;
 
-        
-        if (taskInput.value.trim()) {
-            setTask({ name: taskInput.value, completed: false });
-            
-            setTaskList(prev => [...prev, task]);
-            taskInput.value = "";
-        }
+        // const newTask = { name: taskInput.value, completed: false };
+        // setTaskList(prev => [...prev, newTask]);
+
+        // setTask({name: taskInput.value, completed: false});
+        setTaskList((prev) => [...prev, task]);
+        setTask({ name: "", completed: false });
+        taskInput.value = "";
     };
 
     const handelIsDone = (e: ChangeEvent<HTMLInputElement>) => {
         setCheck(e.target.checked);
         const copy = [...taskList];
-        const arr = copy.filter(task => task.name === e.target.value);
+        const arr = copy.filter((task) => task.name === e.target.value);
         arr[0].completed = e.target.checked;
-        const index = copy.findIndex(task => arr[0].name = task.name);
+        const index = copy.findIndex((task) => (arr[0].name = task.name));
         copy[index] = arr[0];
         setTaskList(copy);
-
-    }
+    };
 
     const handelRemoveTask = (index: number): void => {
         const updatedTask = [...taskList];
         updatedTask.splice(index, 1);
         setTaskList(updatedTask);
-    }
+    };
 
     return (
         <div className="container">
             <h1>TODO List App</h1>
             <div className="input-container">
                 <input
+                    onChange={(e) => {
+                        setTask({ ...task, name: e.target.value });
+                    }}
                     className="form-label"
                     type="text"
                     id="taskInput"
@@ -58,24 +60,25 @@ const TodoList: React.FC = (): JSX.Element => {
                 </button>
             </div>
             <ul id="taskList">
-                {taskList.filter(task => task.name !== '').map((task, index) => (
-                    <li key={index}>
-                        {task.name}
-                        <input onChange={handelIsDone} type="checkbox" id="isDone" />
-                        <button onClick={() => handelRemoveTask(index)}>
-                            Delete
-                        </button>
-                    </li>
-                ))}
+                {taskList
+                    .filter((task) => task.name !== "")
+                    .map((task, index) => (
+                        <li key={index} style={task.completed ? { color: 'green'} : {}}>
+                            {task.name}
+                            <input
+                                onChange={handelIsDone}
+                                type="checkbox"
+                                id="isDone"
+                                value={task.name}
+                            />
+                            <button onClick={() => handelRemoveTask(index)}>
+                                Delete
+                            </button>
+                        </li>
+                    ))}
             </ul>
         </div>
     );
 };
 
 export default TodoList;
-
-
-
-
-
-
