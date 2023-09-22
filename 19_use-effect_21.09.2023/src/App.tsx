705d207b6a7203a4f493e2ec0348f64ed1ac1c9b
Denys Babenko <denchikkooll@gamil.com>
2023-09-22 10:52:46 +0200

    hw 19 update

--- 19_use-effect_21.09.2023/src/App.tsx
+++ 19_use-effect_21.09.2023/src/App.tsx
@@ -2,10 +2,12 @@
 import './App.css';
 import ClassComponents from './components/ClassComponents';
 import Counter from './components/Counter';
+import Users from './components/Users';
 
 function App() {
   return (<>
-  <Counter initialValue={0}/>
+  {/* <Counter initialValue={0}/> */}
+  <Users />
   </>
   );
 }
