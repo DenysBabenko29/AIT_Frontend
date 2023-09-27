9bb101d40fb03112e0cf0a8e6b589b07701db7af
Denys Babenko <denchikkooll@gamil.com>
2023-09-27 21:26:35 +0200

    update hw 21

--- 21_star-wars_26.09.2023/src/components/header/NavItem.tsx
+++ 21_star-wars_26.09.2023/src/components/header/NavItem.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
-import { PageContext } from "../../App";
+// import React, { useContext } from "react";
+// import { PageContext } from "../../App";
 
 
 const NavItem: React.FC<{
     itemTitle: string;
     changePage: (currentPage: string) => void;
 }> = ({ itemTitle, changePage }) => {
-    const myFunction = useContext(PageContext);
+    // const myFunction = useContext(PageContext);
     return (
         <li className="commonButton" onClick={() => changePage(itemTitle)}>
             {itemTitle}
