de267a71de5ce6ea633e8c51ab9c10acf7760270
Denys Babenko <denchikkooll@gamil.com>
2023-10-02 13:04:55 +0200

    redux toolkit

--- 23_redux-intro_28.09.2023/src/App.tsx
+++ 23_redux-intro_28.09.2023/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 // import Counter from './components/Counter';
-import Sandwich from './components/sandwich/Sandwich';
-import Book from './components/Book/Book';
+// import Sandwich from './components/sandwich/Sandwich';
+// import Book from './components/Book/Book';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    // <Counter />
+    <Counter />
     // <Sandwich />
-    <Book />
+    // <Book />
   );
 }
 
--- 23_redux-intro_28.09.2023/src/components/Counter.tsx
+++ 23_redux-intro_28.09.2023/src/components/Counter.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import {useSelector, useDispatch} from 'react-redux'
-import { RootState } from "../redux/store";
+// import { RootState } from "../redux/store";
+import { RootState } from "../redux/storeRTK";
+import { decrement, increment } from "../redux/slices/counterSlice";
 
 const Counter: React.FC = (): JSX.Element => {
 
     const counter = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch()
 
+    // const handleMinus = (): void => {
+    //     dispatch({type: 'counter/minus', payload: 1})
+    // }
+
+    // const handlePlus = (): void => {
+    //     dispatch({type: 'counter/plus', payload: 1})
+    // }
+
     const handleMinus = (): void => {
-        dispatch({type: 'counter/minus', payload: 1})
+        dispatch(decrement())
     }
 
     const handlePlus = (): void => {
-        dispatch({type: 'counter/plus', payload: 1})
+        dispatch(increment(1))
     }
 
     return (
